9834b694cebc119a0d606d9c1f9ac8657fa8e27c
Lcberg-Dao <2023148968@qq.com>
2024-10-18 23:45:27 +0800

    Add files via upload

---
+++ 矩阵计算.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d", &n, &m);
+//
+//	int i = 0;
+//	int a = 0;
+//	int sum = 0;
+//	int ret = n * m;
+//
+//	for (i = 0; i < ret; i++)
+//	{
+//		scanf("%d", &a);
+//		if (a > 0)
+//			sum = sum + a;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
---
+++ 进制转换.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+////Áù½øÖÆ
+//
+//int base(int x)
+//{
+//	if (x >= 6)
+//	{
+//		base(x / 6);
+//		printf("%d", x % 6);	
+//	}
+//	else if (x > 0 && x < 6)
+//		printf("%d", x % 6);		
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	base(n);
+//
+//	return 0;
+//}

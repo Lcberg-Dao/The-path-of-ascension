1f471b1dbc62dc2002b61b542199ed6e5fa012f6
Lcberg-Dao <2023148968@qq.com>
2024-10-19 23:41:37 +0800

    Add files via upload

--- 矩阵转置.c
+++ 矩阵转置.c
@@ -1,1 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int n = 0;
+	int m = 0;
+	int k = n * m;
+	scanf("%d %d", &n, &m);
+	int arr[100];
+
+	input(arr, k, n, m);
+	return 0;
+}
